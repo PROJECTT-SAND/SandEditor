5c5e3bc66c514b8fa3781b0582332933edcafa06
lavi <npt1237@gmail.com>
2022-03-01 00:40:25 +0900

    FEAT : 필요한 페이지 더 추가

---
+++ src/blocks.jsx
@@ -0,0 +1,10 @@
+import style from './style/blocks.module.scss';
+
+function objects() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default objects;
---
+++ src/workspace.jsx
@@ -0,0 +1,10 @@
+import style from './style/workspace.module.scss';
+
+function objects() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default objects;
