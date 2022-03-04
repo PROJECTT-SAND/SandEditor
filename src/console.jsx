b1af46c5e915cf54c55e095c7e6e08e403e1b501
lavi <npt1237@gmail.com>
2022-03-04 21:03:56 +0900

    FEAT : 필요한 구성요소 추가

---
+++ src/console.jsx
@@ -0,0 +1,10 @@
+import style from './style/console.module.scss';
+
+function console() {
+  return (
+    <div className={style.console}>
+    </div>
+  );
+}
+
+export default console;
---
+++ src/folder.jsx
@@ -0,0 +1,10 @@
+import style from './style/folder.module.scss';
+
+function folder() {
+  return (
+    <div className={style.folder}>
+    </div>
+  );
+}
+
+export default folder;
