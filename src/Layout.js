85a816f06410a12d1016fce8205704f820eb17c1
lavi <npt1237@gmail.com>
2022-03-04 21:09:22 +0900

    STYLE : viewerController 컴포넌트 이름 변경 등등

--- src/Layout.js
+++ src/Layout.js
@@ -1,8 +1,11 @@
 import style from './style/layout.module.scss';
 import React, { useState } from 'react';
 import Viewer from './viewer';
+import ViewerController from './viewerController';
 import Objects from './objects';
+import Folder from './folder';
 import Workspace from './workspace';
+import Console from './console';
 
 function Layout() {
   const [leftBottomLayoutSize, setleftBottomLayoutSize] = useState(60);
@@ -13,16 +16,16 @@ function Layout() {
       <div className={style.layout_left}>
         <div className={style.viewer}>
           <div className={style.viewer_viewer}><Viewer /></div>
-          <div className={style.viewer_controller}></div>
+          <div className={style.viewer_controller}><ViewerController /></div>
         </div>
         <div className={style.layout_left_bottom}>
           <div className={style.objects} style={{ width: `${leftBottomLayoutSize}%` }}><Objects /></div>
-          <div className={style.folder}></div>
+          <div className={style.folder}><Folder /></div>
         </div>
       </div>
       <div className={style.layout_right}>
         <div className={style.workspace} style={{ height: `${rightLayoutSize}%` }}><Workspace /></div>
-        <div className={style.console}></div>
+        <div className={style.console}><Console /></div>
       </div>
     </div>
   );
---
+++ src/viewerController.jsx
@@ -0,0 +1,10 @@
+import style from './style/viewerController.module.scss';
+
+function viewerController() {
+  return (
+    <div className={style.viewerController}>
+    </div>
+  );
+}
+
+export default viewerController;
