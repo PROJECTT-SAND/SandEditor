a1d4c63bca8e5b24689d8751ac0cdd56eb4bb417
lavi <npt1237@gmail.com>
2022-03-09 00:39:09 +0900

    FEAT : 폴더 UI 완성

--- src/folder.jsx
+++ src/folder.jsx
@@ -1,5 +1,6 @@
 import style from './style/Folder.module.scss';
 import Window from './Window';
+import example from './example1.png';
 
 function Folder() {
   return (
@@ -7,6 +8,10 @@ function Folder() {
       <div className={style.topBar}>
         <i>C:/users/user/desktop/game</i>
         <button className={style.topBar_back}></button>
+      </div>
+      <div className={style.folder}>
+        <div className={style.file}><img src={example} alt='야쓰'></img><span>1111</span></div>
+        <div className={style.file}><img src={example} alt='야쓰'></img><span>222222</span></div>
       </div>
     </Window>
   );
