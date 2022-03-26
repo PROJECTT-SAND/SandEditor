40647f5b6d2ca11e7938328188eced6437df0f16
lavi <npt1237@gmail.com>
2022-03-27 05:32:17 +0900

    FEAT: objects 컴포넌트 수정 및 기능 추가

--- src/objects.jsx
+++ src/objects.jsx
@@ -1,8 +1,69 @@
+import { useState } from 'react';
 import style from './style/Objects.module.scss';
 import Window from './Window';
-<Window>
-</Window>
+import folderIcon from './res/icon/folder.svg';
+import arrowDownIcon from './res/icon/arrow_down.svg';
+import arrowUpIcon from './res/icon/arrow_up.svg';
+
 function Objects() {
+  const [objectIsOpened, setobjectIsOpened] = useState({
+    InGame: true,
+    Chr1: false
+  });
+
+  const [objectTree, setObjectTree] = useState({
+    'InGame': [
+      'Tlqkf',
+      'Tlqkf2',
+      {'Chr1': [
+        'Head',
+        'arm',
+        'body',
+        'leg',
+        'foot'
+      ]}
+    ]
+  });
+
+  function Object({children, name, icon, isHidden}) {
+
+    const content = (
+      <>
+        <div className={style.object_icon}><img src={icon}></img></div>
+        <span className={style.object_name}>{name}</span>
+        <div className={style.object_see}></div>
+        <div className={style.object_goto}></div>
+      </>
+    );
+
+    if (children !== undefined) {
+      return (
+        <div className={(true) ? style.object_folder1 : style.object_folder2}>
+          <div className={style.object}>
+            <div className={style.object_arrow} onClick={(e) => {
+              setobjectIsOpened({
+                ...objectIsOpened,
+                [name] : !objectIsOpened[name]
+              });
+
+              console.log(e.target.parentNode.parentNode.parentNode.className === style.object_child)
+            }}>
+              <img src={(objectIsOpened[name]) ? arrowDownIcon : arrowUpIcon}></img>
+            </div>
+            {content}
+          </div>
+          {(objectIsOpened[name]) ? <div className={style.object_child}>{children}</div> : null}
+        </div>
+      );
+    } else {
+      return (
+        <div className={style.object}>
+          {content}
+        </div>
+      );
+    }
+  }
+
   return (
     <Window>
       <div className={style.topBar}>
@@ -10,8 +71,19 @@ function Objects() {
         <i>여기에 검색하세요</i>
         <button className={style.topBar_search}></button>
       </div>
-      <img></img>
-      <span></span>
+      <div className={style.objects}>
+        <Object name="InGame" icon={folderIcon} isHidden={false}>
+          <Object name="Tlqkf" icon={folderIcon} isHidden={true} />
+          <Object name="Tlqkf2" icon={folderIcon} isHidden={false} />
+          <Object name="Chr1" icon={folderIcon} isHidden={true}>
+            <Object name="Head" icon={folderIcon} isHidden={true} />
+            <Object name="arm" icon={folderIcon} isHidden={true} />
+            <Object name="body" icon={folderIcon} isHidden={true} />
+            <Object name="leg" icon={folderIcon} isHidden={true} />
+            <Object name="foot" icon={folderIcon} isHidden={true} />
+          </Object>
+        </Object>
+      </div>
     </Window>
   );
 }
