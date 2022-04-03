f53465210a60f82679717b8863493104a4d33bad
lavi <npt1237@gmail.com>
2022-04-03 11:01:36 +0900

    CHORE : electron 추가 및 기본 설정

---
+++ src/main.js
@@ -0,0 +1,55 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const log = require('electron-log');
+
+function createWindow() {
+    const win = new BrowserWindow({ 
+        width: 800, 
+        height: 600, 
+        webPreferences: { 
+            nodeIntegration: true,
+            contextIsolation : false
+        }
+    });
+
+    win.webContents.openDevTools();
+
+    // win.loadFile("build/index.html");
+    win.loadURL(`file://${__dirname}/../build/index.html`);
+
+    const template = [
+        {
+            label: "SAND-editer",
+            submenu: [],
+        },
+        {
+            label: "파일",
+            submenu: [
+                {
+                    label: "파일 만들기",
+                    click: () => {
+                        window.webContents.excuteJavaScript(`
+                            getBackground();
+                        `);
+                    }
+                },
+                {
+                    label: "파일 저장",
+                },
+                {
+                    label: "파일 열기",
+                },
+            ],
+        }
+    ];
+
+    const customMenu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(customMenu);
+}
+
+app.whenReady().then(() => { 
+    createWindow() 
+})
+
+app.on('window-all-closed', () => { 
+    if (process.platform !== 'darwin') app.quit() 
+})
