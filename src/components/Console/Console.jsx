3f3bbe01b2336ba2bc841285d7f5c4fa44089463
lavi <npt1237@gmail.com>
2022-04-09 01:55:05 +0900

    FEAT: 콘솔에 예시 적용 및 스타일링

--- src/components/Console/Console.jsx
+++ src/components/Console/Console.jsx
@@ -9,6 +9,17 @@ export default function Console() {
         <i>Console</i>
         <button className={style.topBar_search}></button>
       </div>
+      <code className={style.console}>
+        <span className={style.target}>&#62; </span>asdgjohgasfdjkhgafjkhgsdoujasgd<br />
+        <span className={style.errer}>오류 - 알 수 없는 명령어</span><br />
+        <span className={style.target}>&#62; </span>sys run<br />
+        시스템: 실행 중<br />
+        시스템: 실행 완료<br />
+        <span className={style.warning}>주의 - <span className={style.object}>InGame</span> 코딩이 이상해유</span><br />
+        <span className={style.warning}>주의 - 코드를 너무 엿같이 짜놨읍니다</span><br />
+        <span className={style.warning}>주의 - 코드를 너무 엿같이 짜놨읍니다</span><br />
+        <span className={style.target}>&#62; </span><span className='cursor'>_</span>
+      </code>
     </Window>
   );
 }
